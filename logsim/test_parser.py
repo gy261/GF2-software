de615c3258825217fb2f31f7da0e31ea0dc5c452
Guo Yang <94389344+gy261@users.noreply.github.com>
2023-05-28 22:39:13 +0100

    Create test_parser.py

---
+++ logsim/test_parser.py
@@ -0,0 +1,13 @@
+# for testing parser
+from parse import Parser
+import names, devices, network, monitors, scanner
+from userint import UserInterface
+names = names.Names()
+devices = devices.Devices(names)
+network = network.Network(names, devices)
+monitors = monitors.Monitors(names, devices, network)
+
+scanner = scanner.Scanner("t1.txt", names)
+p = Parser(names, devices, network, monitors, scanner)
+
+p.parse_network()
