9089213c43d20f8dbccc21c4aee60c41b2c97b56
Guo Yang <94389344+gy261@users.noreply.github.com>
2023-05-31 17:35:00 +0100

    Update test_parser.py

--- logsim/test_parser.py
+++ logsim/test_parser.py
@@ -4,7 +4,8 @@ from names import Names
 from devices import Devices
 from network import Network
 from monitors import Monitors
-from scanner import Scanner, Symbol
+from scanner import Scanner
+
 
 def initialization(text_file):
     names = Names()
@@ -15,6 +16,7 @@ def initialization(text_file):
     p = Parser(names, devices, network, monitors, scanner)
     return p
 
+
 # start to test every separate function in parser
 # test skip_line
 @pytest.mark.parametrize("inputs, outputs", [("pytest_file/t1.txt", "skipped")])
@@ -24,6 +26,7 @@ def test_skip_line(inputs, outputs):
     rt_value = p.skip_line()
     assert rt_value == outputs
 
+
 # test build clock with 0 period
 @pytest.mark.parametrize("inputs, outputs", [("pytest_file/t2.txt", 'Expect period to be larger than 0')])
 def test_clock(inputs, outputs):
@@ -31,6 +34,7 @@ def test_clock(inputs, outputs):
     p.read()
     rt_value = p.build_device()
     assert rt_value[0] == outputs
+
 
 # test build switch with invalid state
 @pytest.mark.parametrize("inputs, outputs", [("pytest_file/t3.txt", "Expect state should be 0(low) 1(high)")])
@@ -40,27 +44,17 @@ def test_switch(inputs, outputs):
     rt_value = p.build_device()
     assert rt_value[0] == outputs
 
+
 # test build device syntax
-@pytest.mark.parametrize("inputs, outputs", [("pytest_file/t4.txt", "Expect stopping sign")])
-def test_build_device_syntax1(inputs, outputs):
+@pytest.mark.parametrize("inputs, outputs",
+                         [("pytest_file/t4.txt", "Expect stopping sign"), ("pytest_file/t5.txt", "A name expected"),
+                          ("pytest_file/t6.txt", "Expect equal sign")])
+def test_build_device_syntax(inputs, outputs):
     p = initialization(inputs)
     p.read()
     rt_value = p.build_device()
     assert rt_value[0] == outputs
 
-@pytest.mark.parametrize("inputs, outputs", [("pytest_file/t5.txt", "A name expected")])
-def test_build_device_syntax2(inputs, outputs):
-    p = initialization(inputs)
-    p.read()
-    rt_value = p.build_device()
-    assert rt_value[0] == outputs
-
-@pytest.mark.parametrize("inputs, outputs", [("pytest_file/t6.txt", "Expect equal sign")])
-def test_build_device_syntax3(inputs, outputs):
-    p = initialization(inputs)
-    p.read()
-    rt_value = p.build_device()
-    assert rt_value[0] == outputs
 
 # test build_connect
 @pytest.mark.parametrize("inputs, outputs", [("pytest_file/t7.txt", "Device called not defined")])
@@ -70,6 +64,7 @@ def test_build_connect(inputs, outputs):
     rt_value = p.build_connect()
     assert rt_value[0] == outputs
 
+
 # test build_monitor
 @pytest.mark.parametrize("inputs, outputs", [("pytest_file/t8.txt", "Device called not defined")])
 def test_build_monitor(inputs, outputs):
@@ -78,24 +73,24 @@ def test_build_monitor(inputs, outputs):
     rt_value = p.build_connect()
     assert rt_value[0] == outputs
 
+
 # Overall testing:
-@pytest.mark.parametrize("inputs, outputs", [("pytest_file/t9.txt", 0)])
-def test_file1(inputs, outputs):
+@pytest.mark.parametrize("inputs, outputs",
+                         [("pytest_file/t9.txt", 0), ("pytest_file/t10.txt", 5), ("pytest_file/t11.txt", 7)])
+def test_file(inputs, outputs):
     p = initialization(inputs)
     p.parse_network()
     rt_value = p.scanner.error_count
     assert rt_value == outputs
 
-@pytest.mark.parametrize("inputs, outputs", [("pytest_file/t10.txt", 5)])
-def test_file2(inputs, outputs):
+
+# Joint testing with team:
+@pytest.mark.parametrize("inputs, outputs",
+                         [("pytest_file/definition_file_1.txt", 0), ("pytest_file/definition_file_2.txt", 0)])
+def test_joint(inputs, outputs):
     p = initialization(inputs)
     p.parse_network()
     rt_value = p.scanner.error_count
     assert rt_value == outputs
 
-@pytest.mark.parametrize("inputs, outputs", [("pytest_file/t11.txt", 7)])
-def test_file3(inputs, outputs):
-    p = initialization(inputs)
-    p.parse_network()
-    rt_value = p.scanner.error_count
-    assert rt_value == outputs
+
